22e71d30175a0bdd7fa051b27defdb76a7ca3a27
JCarlosPV <102228408+JCarlosPV@users.noreply.github.com>
2023-01-19 10:15:25 +0100

    Position + Angle

---
+++ AATorcsV4/AATorcsV2.1/src/QLearning/Constantes.java
@@ -0,0 +1,14 @@
+package QLearning;
+
+public final class Constantes {
+	
+	public final static int NUM_STATES_STEER = 16;
+	public final static float[] STEER_VALUES = {-0.2f, -0.1f, 0f, 0.1f, 0.2f};
+	public final static int NUM_ANGLES = STEER_VALUES.length;
+	
+	private Constantes() {
+		
+	}
+	
+	
+}
