7dad154000ccaed9df1a3f3e716e8b379420ce4b
JCarlosPV <102228408+JCarlosPV@users.noreply.github.com>
2023-01-29 00:55:54 +0100

    Falta definir estados y recompensa

---
+++ QLearning-TORCS.v4/src/QLearning/Test.java
@@ -0,0 +1,26 @@
+package QLearning;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		QTable qtable_velocidad = new QTable("Velocidad", Constantes.NUM_STATES_VEL, Constantes.NUM_VEL,
+				Constantes.VEL_VALUES);
+		qtable_velocidad.loadQTable("qtable_velocidad");
+		QTable qtable_steer = new QTable("Volante",Constantes.NUM_STATES_STEER, Constantes.NUM_ANGLES, Constantes.STEER_VALUES);
+		qtable_steer.loadQTable("qtable_volante");
+		
+		Politica.savePolitica("volante", qtable_steer, Constantes.STEER_VALUES);
+		Politica.savePolitica("velocidad", qtable_velocidad, Constantes.VEL_VALUES);
+		
+		System.out.println("###########");
+		Politica p = new Politica();
+		//p.loadPolitica("volante");
+		
+		System.out.println("###########");
+		p.loadPolitica("velocidad");	
+	}
+
+}
